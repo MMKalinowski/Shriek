be331ed978e041ef7c8edb5995038aeaa9d7daee
Maciej Mateusz Kalinowski <m.kalinowski@ncl.ac.uk>
2019-03-25 14:05:35 +0000

    added FPS counter

---
+++ Assets/FPSCount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCount : MonoBehaviour
+{
+    public Text fpstxt;
+
+    float deltaTime = 0f;
+    float fps = 0f;
+
+    public void Update()
+    {
+        deltaTime += Time.deltaTime;
+        deltaTime /= 2.0f;
+        fps = 1.0f / deltaTime;
+
+        fpstxt.text = ((int)fps).ToString();
+    }
+}
